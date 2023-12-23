d00428ac42b1b072ef1d1bf8f44b2fbe5fa0334f
painguen <tatarunononu@gmail.com>
2023-12-23 07:49:10 -0600

    Fixed display issues. Now new issues. With help from my friend nino (thanks nino), there was fixes in getting it to display but now I need to figure out how to make inputting work. (Also the moving cursor thing is neat)

--- Farmin.c
+++ Farmin.c
@@ -11,40 +11,36 @@ int plottwo = 0;
 int plotthree = 0;
 int plotfour = 0;
 
-char getCharacter()
+void move_relative(int y, int x)
 {
-    char c = getchar();
-    int b = 0;
-    do
-    {
-        b = getchar();
-    } while (b != EOF && b != '\n');
-    return c;
+    int current_y;
+    int current_x;
+    getsyx(current_y, current_x);
+    move(current_y + y, current_x + x);
 }
 
 int growing(){
     printw("You have planted your crop\n");
     printw(".");
-    sleep(10);
     refresh();
+    sleep(3);
+    move_relative(0, -1);
     printw("t");
-    sleep(15);
     refresh();
+    sleep(3);
+    move_relative(0, -1);
     printw("Y");
-    sleep(5);
     refresh();
-    printw("You crop has finished growing.\n");
+    sleep(5);
+    printw("\nYou crop has finished growing.\n");
     crops++;
 }
 
 int main(){
-char playerInput;
-playerInput = getCharacter();
 initscr(); // Starts the screen
-printw("Open plots\nPlot One");
-refresh();
-printw("Coins : %i | Crops : %i",coins,crops);
-refresh();
+int playerInput = 1;
+printw("Coins : %i | Crops : %i\n",coins,crops);
+printw("Open plots\nPlot One\n");
 printw("To select a plot input it's number\n");
 refresh();
 if (playerInput==1){
